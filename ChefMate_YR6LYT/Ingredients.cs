f53bd9528e4ffbc514e439a10ab2e6aa3ad24dc6
Csanad <csanilajko@gmail.com>
2025-10-20 22:51:20 +0200

    Added main recipe and ingredient classes

---
+++ ChefMate_YR6LYT/Ingredients.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ChefMate_YR6LYT
+{
+    public partial class Ingredients : ObservableObject
+    {
+        [ObservableProperty]
+        int id;
+
+        [ObservableProperty]
+        int recipeId;
+
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        string description;
+
+        [ObservableProperty]
+        string quantity;
+
+        [ObservableProperty]
+        DateTime createdAt;
+
+        [ObservableProperty]
+        string imapePath;
+    }
+}
---
+++ ChefMate_YR6LYT/Recipes.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefMate_YR6LYT
+{
+    public partial class Recipes : ObservableObject
+    {
+        [ObservableProperty]
+        int id;
+
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        string description;
+
+        [ObservableProperty]
+        DateTime createdAt;
+
+        [ObservableProperty]
+        DateTime updatedAt;
+
+        [ObservableProperty]
+        int preparationTimeMinutes;
+
+        [ObservableProperty]
+        string difficulty;
+
+        [ObservableProperty]
+        double longtitude;
+
+        [ObservableProperty]
+        double latitude;
+    }
+}
