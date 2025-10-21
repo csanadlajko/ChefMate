7557ef747e90fa1f96c005661aafe798d276eea5
Csanad <csanilajko@gmail.com>
2025-10-21 19:16:32 +0200

    Added interface and class for sqlite db handling

---
+++ ChefMate_YR6LYT/IChefMateDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefMate_YR6LYT
+{
+    public interface IChefMateDatabase
+    {
+        // Create new recipe
+        Task AddRecipeAsync(Recipes recipe);
+
+        // Read all recipes
+        Task<List<Recipes>> GetAllRecipesAsync();
+
+        // Read a specific recipe by ID
+        Task<Recipes> GetRecipeAsync(int id);
+
+        // Update existing recipe
+        Task UpdateRecipeAsync(Recipes recipe);
+
+        // Delete recipe by ID
+        Task DeleteRecipeAsync(int id);
+
+        // Add ingredient to recipe
+        Task AddIngredientAsync(Ingredients ingredient);
+
+        // Get ingredients for a specific recipe
+        Task<List<Ingredients>> GetIngredientsForRecipeAsync(int recipeId);
+
+        // Update ingredient under a recipe
+        Task UpdateIngredientAsync(Ingredients ingredient);
+
+        // Delete ingredient by ID
+        Task DeleteIngredientAsync(int id);
+    }
+}
---
+++ ChefMate_YR6LYT/SQLiteChefMateDatabase.cs
@@ -0,0 +1,72 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChefMate_YR6LYT
+{
+    public class SQLiteChefMateDatabase : IChefMateDatabase
+    {
+        SQLiteAsyncConnection _database;
+
+        public SQLiteChefMateDatabase(SQLiteAsyncConnection database)
+        {
+            this._database = database;
+            // the tables below should be created in the viewmodel constructor
+            //_database.CreateTableAsync<Recipes>().Wait();
+            //_database.CreateTableAsync<Ingredients>().Wait();
+        }
+
+        public async Task AddIngredientAsync(Ingredients ingredient)
+        {
+            _database.Table<Ingredients>();
+            await _database.InsertAsync(ingredient);
+        }
+
+        public async Task AddRecipeAsync(Recipes recipe)
+        {
+            _database.Table<Recipes>();
+            await _database.InsertAsync(recipe);
+        }
+
+        public async Task DeleteIngredientAsync(int id)
+        {
+            await _database.DeleteAsync<Ingredients>(id);
+        }
+
+        public async Task DeleteRecipeAsync(int id)
+        {
+            await _database.DeleteAsync<Recipes>(id);
+        }
+
+        public async Task<List<Recipes>> GetAllRecipesAsync()
+        {
+            return await _database.Table<Recipes>().ToListAsync();
+        }
+
+        public async Task<List<Ingredients>> GetIngredientsForRecipeAsync(int recipeId)
+        {
+            return await _database.Table<Ingredients>().ToListAsync();
+        }
+
+        public async Task<Recipes> GetRecipeAsync(int id)
+        {
+            _database.Table<Recipes>();
+            return await _database.GetAsync<Recipes>(id);
+        }
+
+        public async Task UpdateIngredientAsync(Ingredients ingredient)
+        {
+            _database.Table<Ingredients>();
+            await _database.UpdateAsync(ingredient);
+        }
+
+        public async Task UpdateRecipeAsync(Recipes recipe)
+        {
+            _database.Table<Recipes>();
+            await _database.UpdateAsync(recipe);
+        }
+    }
+}
